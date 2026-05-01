21fddcbed08140bb1749d4e123dec8ec3aff7d8a
Sanooda Abeysinghe <168185705+sanuwa13@users.noreply.github.com>
2026-05-01 20:02:12 +0530

    fixed the ticket issue

--- backend/src/main/java/com/smartcampus/backend/repository/TicketRepository.java
+++ backend/src/main/java/com/smartcampus/backend/repository/TicketRepository.java
@@ -3,9 +3,11 @@ package com.smartcampus.backend.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.smartcampus.backend.model.Ticket;
+import com.smartcampus.backend.model.User;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findByAssignedTo(String assignedTo);
     List<Ticket> findByAssignedToAndStatus(String assignedTo, String status);
     Ticket findTopByOrderByIdDesc();
+    List<Ticket> findByCreatedBy(User createdBy);
 }
